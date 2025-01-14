a76d76e416994127565766bd0cca83d24b8eb523
emrepektemek <okyanus5401@hotmail.com>
2025-01-14 15:31:26 +0300

    Loading Spinner Eklendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product';
 import { ProductResponseModel } from './../../models/productResponseModel';
-import { HttpClient } from '@angular/common/http';
+import { ProductService } from './../../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -12,19 +12,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  apiUrl = 'https://localhost:44372/api/products/getall';
+  dataLoaded: boolean = false;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts() {
-    this.httpClient
-      .get<ProductResponseModel>(this.apiUrl)
-      .subscribe((response) => {
-        this.products = response.data;
-      });
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
   }
 }
