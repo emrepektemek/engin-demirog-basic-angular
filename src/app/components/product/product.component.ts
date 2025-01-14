716761be9af9f8b30fdd34467342ace64b3541e2
emrepektemek <okyanus5401@hotmail.com>
2025-01-14 20:17:09 +0300

    filter-pipe eklendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product';
 import { ProductService } from './../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { VatAddedPipe } from '../../pipes/vat-added.pipe';
+import { FormsModule } from '@angular/forms';
+import { FilterPipePipe } from '../../pipes/filter-pipe.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule],
+  imports: [CommonModule, VatAddedPipe, FormsModule, FilterPipePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded: boolean = false;
+  filterText: string = '';
 
   constructor(
     private productService: ProductService,
